d2a0860c8eba44013b8f3737b8b04c2d07f30147
Amish Mathais <amishmathais@gmail.com>
2026-04-12 17:08:01 +0530

    First version of the cli version

--- game/Game.java
+++ game/Game.java
@@ -1,8 +1,46 @@
 package game;
 
+import java.util.Scanner;
+
 public class Game {
     public static void mainGame() {
-        Utils.clearScreen();
-        System.out.println("Game Started");
+	Scanner input = new Scanner(System.in);
+	
+        boolean run = true;
+	while (run) {
+	    System.out.println("Enter number of disks: ");
+	    int numDisks = input.nextInt();
+
+	    Utils.clearScreen();
+
+	    GameState currState = new GameState(numDisks);
+
+	    while (!currState.isGameWon()) {
+		Utils.printState(currState.getTowers(), numDisks);
+
+		System.out.println("Enter rod number to move from (1 = left, 2 = middle, 3 = right): ");
+		int fromRod = input.nextInt();
+	    
+		System.out.println("Enter rod number to move to (1 = left, 2 = middle, 3 = right): ");
+		int toRod = input.nextInt();
+		
+		Utils.clearScreen();
+		if (!currState.move(fromRod, toRod)) {
+		    System.out.println("Invalid move\n");
+		    continue;
+		}
+	    }
+
+	    Utils.printState(currState.getTowers(), numDisks);
+	    
+	    System.out.println("Congratulations!!! You solved it!");
+	    System.out.println("Do you want to play again? (y/n)");
+	    String response = input.next();
+
+	    if (response.toLowerCase().equals("n"))
+		run = false;
+	    else
+		Utils.clearScreen();
+	}
     }   
 }
--- game/Utils.java
+++ game/Utils.java
@@ -1,9 +1,59 @@
 package game;
 
+import java.util.Collections;
+import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
 public class Utils {
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
-        
+
+    public static void printState(List<Stack<Integer>> towers, int numDisks) {
+	int height = numDisks + 1;
+	int maxWidth = 2 * numDisks + 1;
+
+	List<List<Integer>> towersCopy = new ArrayList<>();
+	for (int i = 0; i < 3; i++) {
+	    towersCopy.add(new ArrayList<>());
+
+	    for (int e : towers.get(i))
+		towersCopy.get(i).add(e);
+
+	    Collections.reverse(towersCopy.get(i));
+	}
+	
+	List<Iterator<Integer>> it = new ArrayList<>();
+	for (int i = 0; i < 3; i++)
+	    it.add(towersCopy.get(i).iterator());
+	
+	for (int lineNumber = 1; lineNumber <= height; lineNumber++) {
+	    for (int rod = 0; rod < 3; rod++) {
+		System.out.print(" ");
+
+		int inThisRod = towers.get(rod).size();
+		int currWidth = 1;
+		char c = '|';
+		
+		if (lineNumber > height - inThisRod) {
+		    currWidth = 2 * it.get(rod).next() + 1;
+		    c = '*';
+		}
+
+		int numSpace = (maxWidth - currWidth) / 2;
+		for (int i = 0; i < numSpace; i++)
+		    System.out.print(" ");
+
+		for (int i = 0; i < currWidth; i++)
+		    System.out.print(c);
+		
+		for (int i = 0; i < numSpace; i++)
+		    System.out.print(" ");		
+	    }
+	    System.out.println();
+	}
+    }
 }
